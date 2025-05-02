b5395d0d8bba545643642a6030bf1c764792efad
alvien salvador <biboy@alviens-MacBook-Pro-2.local>
2025-05-02 22:08:45 +1200

    added new data structure

--- ws.hs
+++ ws.hs
@@ -3,12 +3,21 @@ import System.Environment
 import System.IO  
 import Control.Monad
 
+data Operator = Data
+
+main :: IO ()
 main = do 
         args <- getArgs
         contents <- readFile (head args)
-        writeFile "output.txt" (process contents) -- static output file name just for demonstration     
+        let newName = formatFileName (head args)
+        writeFile newName (process contents)     
 
+process :: [Char] -> [Char]
 process input = "START\n" ++ input ++ "\nEND"
 
+formatFileName :: String -> String
+formatFileName s = "output-"++ drop 1 ( dropWhile (/= '-') s)
 
-
+eval :: [Char] -> Operator
+eval [] = ()
+eval (s:sx) = 
