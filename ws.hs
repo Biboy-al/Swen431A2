53146d5585c87224cb9637204a827047a922728e
alvien salvador <biboy@alviens-MacBook-Pro-2.local>
2025-05-03 16:28:57 +1200

    stack new consumes basic operators

--- ws.hs
+++ ws.hs
@@ -4,6 +4,7 @@ import System.IO
 import Control.Monad
 
 data StackOpe = Push Char
+        | Add
         
 main :: IO ()
 main = do 
@@ -17,13 +18,22 @@ main = do
 process :: [Char] -> [Char]
 process input = "START\n" ++ input ++ "\nEND"
 
+-- Formats the name of the file to have output at the start
 formatFileName :: String -> String
 formatFileName s = "output-"++ drop 1 ( dropWhile (/= '-') s)
 
+-- Performs operaton on the stack based the command
 performOp:: StackOpe -> [Char] -> [Char]
 performOp (Push n) s = n : s
+performOp Add (o1:o2:s) = o1: o2 : s 
 
+-- Checks if a char is an operator
+isOperator :: Char -> Bool
+isOperator c = c `elem` "+-*/"
+
+-- Main Evaluation function for the stack
 eval:: [Char] -> [Char] -> [Char]
-
 eval [] s = s
-eval (o:ox) s = eval ox (performOp (Push o) s)
+eval (o:ox) s 
+        | isOperator o = eval ox (performOp Add s)
+        | otherwise = eval ox (performOp (Push o) s)
