59845b110f18bf4a489095794a6aa4b2dd45d5b5
alvien salvador <biboy@alviens-MacBook-Pro-2.local>
2025-05-03 12:57:29 +1200

    added push functionality

--- ws.hs
+++ ws.hs
@@ -3,14 +3,16 @@ import System.Environment
 import System.IO  
 import Control.Monad
 
-data Operator = Data
-
+data StackOpe = Push Char
+        
 main :: IO ()
 main = do 
         args <- getArgs
         contents <- readFile (head args)
         let newName = formatFileName (head args)
-        writeFile newName (process contents)     
+        let stack = eval (process contents) []
+        writeFile newName stack
+        
 
 process :: [Char] -> [Char]
 process input = "START\n" ++ input ++ "\nEND"
@@ -18,6 +20,10 @@ process input = "START\n" ++ input ++ "\nEND"
 formatFileName :: String -> String
 formatFileName s = "output-"++ drop 1 ( dropWhile (/= '-') s)
 
-eval :: [Char] -> Operator
-eval [] = ()
-eval (s:sx) = 
+performOp:: StackOpe -> [Char] -> [Char]
+performOp (Push n) s = n : s
+
+eval:: [Char] -> [Char] -> [Char]
+
+eval [] s = s
+eval (o:ox) s = eval ox (performOp (Push o) s)
